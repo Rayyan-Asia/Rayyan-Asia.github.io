764a204407f8d37c4668cbc410461ab08e919c30
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2024-12-17 22:49:37 +0200

    Update for 2024

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,24 @@ $(function()
 {
     new WOW().init();
     $(".rotate").textrotator();
-})
+});
+
+// Function to calculate age
+function calculateAge(birthday) {
+    const today = new Date();
+    const birthDate = new Date(birthday);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    // Adjust if birthday hasn't occurred yet this year
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+// DOM Ready Event to Set Age
+$(document).ready(function () {
+    const age = calculateAge('2001-09-23');
+    $('#age').text(age);
+});
